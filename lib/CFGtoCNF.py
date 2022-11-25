a4bee5156df31d11970b6e4ab4a3dfc948af7679
blixa-rd <16521074@mahasiswa.itb.ac.id>
2022-11-25 14:16:38 +0700

    update CFG to CNF

--- lib/CFGtoCNF.py
+++ lib/CFGtoCNF.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 import string
 import codecs
 
-def readCFGFile(filepath):
+def readCFG(filepath):
 	cfg = {} 
 	f = open(filepath, 'r')
 	lines = [line.split('->') for line in f.readlines()]
@@ -19,13 +19,13 @@ def readCFGFile(filepath):
 	f.close()
 	return cfg
 
-def isVar(x):
+def isVariable(x):
 	for char in x:
 		if char not in (string.ascii_uppercase + '_' + string.digits):
 			return False
 	return True
 
-def removeUnitProduction(cfg):
+def delUnitProduction(cfg):
 	keyVar = list(cfg.keys())
 	for var in cfg:
 		productions = cfg[var]
@@ -33,7 +33,7 @@ def removeUnitProduction(cfg):
 		while flag:
 			flag = False
 			for production in productions:
-				if len(production) == 1 and isVar(production[0]):
+				if len(production) == 1 and isVariable(production[0]):
 					productions.remove(production)
 					if production[0] in keyVar:
 						newProduction = deepcopy([production for production in cfg[production[0]] if production not in productions])
@@ -41,25 +41,25 @@ def removeUnitProduction(cfg):
 					flag = True
 	return(cfg)
 
-def get_key(val,cfg):
+def getKey(val, cfg):
     for key, value in cfg.items():
          if val == value:
              return key
 
-def CFGtoCNF(cfg):
+def converter(cfg):
 	temp = {}
 	terminal = []
 	for var in cfg:
 		productions = cfg[var]
 		for production in productions:
-			if len(production) == 1 and not(isVar(production[0])):
+			if len(production) == 1 and not(isVariable(production[0])):
 				terminal.append(production[0])
 	t = 1
 	for var in cfg:
 		productions = cfg[var]
 		for production in productions:
 			for symbol in production:
-				if not(isVar(symbol)):
+				if not(isVariable(symbol)):
 					if symbol not in terminal:
 						terminal.append(symbol)
 						temp.update({f'T{t}' : [[symbol]]})
@@ -71,14 +71,14 @@ def CFGtoCNF(cfg):
 		productions = cfg[var]
 		for i, production in enumerate(productions):
 			for j, symbol in enumerate(production):
-				if not (isVar(symbol)) and len(production) > 1:
-					v = get_key([[symbol]],cfg)
+				if not (isVariable(symbol)) and len(production) > 1:
+					v = getKey([[symbol]],cfg)
 					if not(v):
-						v =  get_key([[symbol]],temp)
+						v =  getKey([[symbol]],temp)
 						if not(v):
 							temp.update({f'T{t}' : [[symbol]]})
 							t += 1
-						productions[i][j] = get_key([[symbol]],temp)
+						productions[i][j] = getKey([[symbol]],temp)
 					else:
 						productions[i][j] = v 
 					cfg.update({var : productions})
@@ -99,8 +99,8 @@ def CFGtoCNF(cfg):
 	cfg.update(temp)
 	return cfg
 
-# cfg = readCFGFile(r'data\cfg.txt')
-# cnf = CFGtoCNF((removeUnitProduction(cfg)))
+# cfg = readCFG(r'data\cfg.txt')
+# cnf = Converter((delUnitProduction(cfg)))
 
 # with codecs.open(r'cnf.txt', mode="w", encoding="utf-8") as f:
 #     for key in cnf:
