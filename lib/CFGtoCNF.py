a9a82f547c5d04c3760b746c4a65ffd9783f02cb
blixa-rd <16521074@mahasiswa.itb.ac.id>
2022-11-25 21:21:36 +0700

    update cfg and converter

--- lib/CFGtoCNF.py
+++ lib/CFGtoCNF.py
@@ -1,111 +1,79 @@
-from copy import deepcopy
-import string
-import codecs
+RULE_DICT = {}
 
-def readCFG(filepath):
-	cfg = {} 
-	f = open(filepath, 'r')
-	lines = [line.split('->') for line in f.readlines()]
-	for line in lines:
-		if line[0] not in "\n" and line[0][0] not in "#":
-			var = line[0].replace(" ","")
-			production = [raw.split() for raw in line[1].split('|')]
-			key = list(cfg.keys())
-			if var in key:
-				productions = cfg[var]
-				productions.extend(production)
-			else:
-				cfg.update({var : production})
-	f.close()
-	return cfg
+def print_grammar(grammar):
+    for rule in grammar:
+        print(rule[0], " -> ", end='')
+        for i in rule[1:]:
+            print(i, end=' ')
+        print()
 
-def isVariable(x):
-	for char in x:
-		if char not in (string.ascii_uppercase + '_' + string.digits):
-			return False
-	return True
+def write_grammar(grammar):
+    file = open('cnf_grammar.txt', 'w')
+    for rule in grammar:
+        file.write(rule[0])
+        file.write(" -> ")
+        for i in rule[1:]:
+            file.write(i)
+            file.write(" ")
+        file.write("\n")
+    file.close()
 
-def delUnitProduction(cfg):
-	keyVar = list(cfg.keys())
-	for var in cfg:
-		productions = cfg[var]
-		flag = True
-		while flag:
-			flag = False
-			for production in productions:
-				if len(production) == 1 and isVariable(production[0]):
-					productions.remove(production)
-					if production[0] in keyVar:
-						newProduction = deepcopy([production for production in cfg[production[0]] if production not in productions])
-						productions.extend(newProduction)
-					flag = True
-	return(cfg)
+def read_grammar(grammar_file):
 
-def getKey(val, cfg):
-    for key, value in cfg.items():
-         if val == value:
-             return key
+    with open(grammar_file) as cfg:
+        baris = cfg.readlines()
+    return [x.replace("->", "").split() for x in baris]
 
-def converter(cfg):
-	temp = {}
-	terminal = []
-	for var in cfg:
-		productions = cfg[var]
-		for production in productions:
-			if len(production) == 1 and not(isVariable(production[0])):
-				terminal.append(production[0])
-	t = 1
-	for var in cfg:
-		productions = cfg[var]
-		for production in productions:
-			for symbol in production:
-				if not(isVariable(symbol)):
-					if symbol not in terminal:
-						terminal.append(symbol)
-						temp.update({f'T{t}' : [[symbol]]})
-						t += 1
-	cfg.update(temp)
-	temp.clear()
+def add_rule(rule):
+ 
+    global RULE_DICT
 
-	for var in cfg:
-		productions = cfg[var]
-		for i, production in enumerate(productions):
-			for j, symbol in enumerate(production):
-				if not (isVariable(symbol)) and len(production) > 1:
-					v = getKey([[symbol]],cfg)
-					if not(v):
-						v =  getKey([[symbol]],temp)
-						if not(v):
-							temp.update({f'T{t}' : [[symbol]]})
-							t += 1
-						productions[i][j] = getKey([[symbol]],temp)
-					else:
-						productions[i][j] = v 
-					cfg.update({var : productions})
-	cfg.update(temp)
-	temp.clear()	
+    if rule[0] not in RULE_DICT:
+        RULE_DICT[rule[0]] = []
+    RULE_DICT[rule[0]].append(rule[1:])
 
-	for var in cfg:
-		productions = cfg[var]
-		idx = 1
-		for i, production in enumerate(productions):
-			while len(production) > 2:
-				temp.update({f"{var}{idx}": [[production[0], production[1]]]})
-				production = production[1:]
-				production[0] = f"{var}{idx}"
-				idx += 1
-			productions[i] = production
-			cfg.update({var : productions})
-	cfg.update(temp)
-	return cfg
+def convert_grammar(grammar):
 
-# cfg = readCFG(r'data\cfg.txt')
-# cnf = Converter((delUnitProduction(cfg)))
+    global RULE_DICT
 
-# with codecs.open(r'cnf.txt', mode="w", encoding="utf-8") as f:
-#     for key in cnf:
-#         for rules in cnf[key]:
-#             prods_str = map(lambda p: "".join(p), rules)
-#             line = f"{key} -> {' | '.join(prods_str)}\n"
-#             line = line.replace("'", "")
-#             f.write(line)
+    unit_productions, res = [], []
+    index = 0
+
+    for rule in grammar:
+        new_rules = []
+        if len(rule) == 2 and rule[1][0] != "'":
+            unit_productions.append(rule)
+            add_rule(rule)
+            continue
+        elif len(rule) > 2:
+            terminals = [(item, i) for i, item in enumerate(rule) if item[0] == "'"]
+            if terminals:
+                for item in terminals:
+                    rule[item[1]] = f"{rule[0]}{str(index)}"
+                    new_rules += [f"{rule[0]}{str(index)}", item[0]]
+                index += 1
+            while len(rule) > 3:
+                new_rules.append([f"{rule[0]}{str(index)}", rule[1], rule[2]])
+                rule = [rule[0]] + [f"{rule[0]}{str(index)}"] + rule[3:]
+                index += 1
+        if rule:
+            add_rule(rule)
+            res.append(rule)
+        if new_rules:
+            for i in range(len(new_rules)):
+                   res.append(new_rules[i])
+
+    while unit_productions:
+        rule = unit_productions.pop()
+        if rule[1] in RULE_DICT:
+            for item in RULE_DICT[rule[1]]:
+                new_rule = [rule[0]] + item
+                if len(new_rule) > 2 or new_rule[1][0] == "'":
+                    res.append(new_rule)
+                else:
+                    unit_productions.append(new_rule)
+                add_rule(new_rule)
+    return res
+
+write_grammar(convert_grammar(read_grammar(r'data\cfg.txt')))
+print(convert_grammar(read_grammar(r'data\cfg.txt')))
